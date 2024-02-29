d5aaf8f8c39e750975ac5eb5c28d55ba957bfed4
OĞULCAN DOĞAN <ogulcan.dgn@hotmail.com>
2024-02-29 19:49:32 +0300

    create rtk slices

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import CarForm from "./components/CarForm";
+import CarValue from "./components/CarValue";
+import CarSearch from "./components/CarSearch";
+import CarList from "./components/CarList";
+
 function App() {
-  return <div>deneme</div>;
+  return (
+    <div>
+      <CarForm />
+      <CarList />
+      <CarSearch />
+      <CarValue />
+    </div>
+  );
 }
 
 export default App;
