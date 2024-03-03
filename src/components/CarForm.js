5e0fcdcba8986cbfed5be9fc612a0f6bec6c5642
OĞULCAN DOĞAN <ogulcan.dgn@hotmail.com>
2024-03-03 23:30:06 +0300

    create carForm form

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 const CarForm = () => {
-  return <div>CarForm</div>;
+  return (
+    <div className="car-form panel">
+      <h4 className="subtitle is-3">Add Car</h4>
+      <form>
+        <div className="field-group">
+          <div className="field">
+            <label className="label">Name</label>
+            <input
+              className="input is-expanded"
+              value={name}
+              onChange={handleNameChange}
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default CarForm;
